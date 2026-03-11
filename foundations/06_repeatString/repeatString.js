bd5122f5b7a94bb4cf338680162bdb07f33a5071
hal <shinycelebi@protonmail.com>
2026-03-10 22:09:51 -0400

    Finish up to ex. 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let output = '';
+  let i = 0;
+  
+  if (num >= 0) {
+    while (i < num) {
+      output += string;
+      i++;
+    }
+    return output;
+  }
 
+  else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
