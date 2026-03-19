aa3969790aae56465549cb699ab888875704cd3f
hal <shinycelebi@protonmail.com>
2026-03-19 16:18:13 -0400

    Make new repeat string function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,19 +1,17 @@
-const repeatString = function(string, num) {
-  let output = '';
-  let i = 0;
-  
-  if (num >= 0) {
-    while (i < num) {
+function repeatString(string, num) {
+  output = "";
+
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
       output += string;
-      i++;
     }
-    return output;
+  }
+  else {
+    return "ERROR";
   }
 
-  else {
-    return 'ERROR';
-  }
-};
+  return output;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
