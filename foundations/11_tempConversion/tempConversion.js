adbc4b93a4dcbfe837381ad011413085c66c31fc
hal <shinycelebi@protonmail.com>
2026-03-19 19:45:35 -0400

    Add basic conversion functionality

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * (5/9);
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (9/5) * tempC + 32
+  return tempF;
 };
 
 // Do not edit below this line
