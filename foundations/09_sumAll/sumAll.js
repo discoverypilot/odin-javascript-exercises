b8175eceb85d1bb71b52d8a8d43ce40f92b68c90
hal <shinycelebi@protonmail.com>
2026-03-19 19:16:41 -0400

    Create sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  if (typeof n1 != 'number'
+   || typeof n2 != 'number'
+   || n1 < 0
+   || n2 < 0
+   || n1 % 1 != 0
+   || n2 % 1 != 0) {
+    return 'ERROR'
+   }
 
+  let count = 0;
+  let min = (n1 <= n2) ? n1 : n2;
+  let max = (n1 >= n2) ? n1 : n2;
+
+  for (min; min <= max; min++) {
+    count += min;
+  }
+
+  return count;
 };
 
 // Do not edit below this line
