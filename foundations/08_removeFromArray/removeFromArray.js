392863b5879469af5ab9d5b2e1ec2da654009dc0
hal <shinycelebi@protonmail.com>
2026-03-19 18:50:29 -0400

    Add array removal filter function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+  return arr.filter(noMatches);
+
+  function noMatches(element) {
+    for (param of toRemove) {
+      if (param === element) {
+        return false;
+      }
+    }
+    return true;
+  }
 };
 
 // Do not edit below this line
