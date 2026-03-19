d4329cd87532e9ed71621d58924d69d87824ae09
hal <shinycelebi@protonmail.com>
2026-03-19 19:48:04 -0400

    Add rounding

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
 const convertToCelsius = function(tempF) {
   let tempC = (tempF - 32) * (5/9);
+  tempC = round(tempC);
   return tempC;
 };
 
 const convertToFahrenheit = function(tempC) {
   let tempF = (9/5) * tempC + 32
+  tempF = round(tempF);
   return tempF;
 };
+
+const round = (x) => Math.round(x * 10) / 10
 
 // Do not edit below this line
 module.exports = {
