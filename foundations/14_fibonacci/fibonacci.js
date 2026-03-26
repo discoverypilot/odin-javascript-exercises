8de8af3e063461b951829b9b2220075f372d518e
hal <shinycelebi@protonmail.com>
2026-03-26 14:05:33 -0400

    Add fibonacci calculator

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+  if (x < 0) {
+    return "OOPS";
+  }
+  
+  let results = [0, 1];
+  for (i = 1; i < x; i++) {
+    results.push(results[i] + results[i-1]);
+  }
+  return results[x];
 };
 
 // Do not edit below this line
