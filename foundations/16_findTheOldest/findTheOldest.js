f1e9bcbbeeecc8bcf57a6c40717db0a76fef265d
hal <shinycelebi@protonmail.com>
2026-03-26 14:53:11 -0400

    Create function to find oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const yearCurrent = new Date(Date.now()).getFullYear();
 
+  return arr.reduce((oldest, person) => {
+    let oldestAge = getAge(oldest);
+    let personAge = getAge(person);
+
+    function getAge(a) {
+      return (!a.yearOfDeath) ? yearCurrent - a.yearOfBirth : a.yearOfDeath - a.yearOfBirth;
+    }
+
+    return (oldestAge >= personAge) ? oldest : person;
+  }, {});
 };
 
 // Do not edit below this line
