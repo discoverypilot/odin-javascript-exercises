2900a3c9476aec6af1170a06da890dcf44130741
hal <shinycelebi@protonmail.com>
2026-03-26 13:46:07 -0400

    Add palindrome checker

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const validChars = "abcdefghijklmnopqrstuvwxyz1234567890";
+  
+  let forwards = string.toLowerCase().split('').filter((a) => validChars.includes(a)).join('');
+  let backwards = forwards.split('').reverse().join('');
 
+  return (backwards == forwards);
 };
 
 // Do not edit below this line
